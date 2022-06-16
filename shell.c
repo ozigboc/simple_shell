d65311e66750722c9b6a19180d2e195bacde5ccb
Muyiwaa <enochmwilliams@gmail.com>
2022-06-16 01:53:18 -0700

    fixed

--- shell.c
+++ shell.c
@@ -1,87 +1,40 @@
 #include "holberton.h"
-
 /**
- * execute - executes the command
- * @cmd: command to run
- * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
+ * main - runs the shell program
+ *
+ * Return: 0 on success
  */
-int execute(char **cmd)
+int main(void)
 {
+    char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
+    char *PATH = NULL;
+    char **av;
+    int exitstatus = 0;
 
-	pid_t child_pid;
-	int status;
-
-	if (strncmp("exit", cmd[0], 4) == 0)
-		return (-1);
-
-	child_pid = fork();
-
-	if (child_pid == -1)
-	{
-		perror("Error");
-		return (1);
-	}
-	else if (child_pid == 0)
-	{
-		if (execve(cmd[0], cmd, NULL) == -1)
-		{
-			perror("Error");
-			exit(-1);
-		}
-	}
-	else
-		wait(&status);
-
-	return (0);
-}
-
-
-/**
- * main - main simple shell
- * @argc: number of arguments
- * @argv: list of command line arguments
- * Return: Always 0, -1 on error.
- */
-
-int main(int argc, char **argv)
-{
-
-	int response;
-	char **tokens;
-	size_t bufsize = BUFSIZ;
-	int isPipe = 0;
-	char *buffer;
-
-	if (argc >= 2)
-	{
-		/*TODO: Handle cases where there is no argument, only the command*/
-		if (execve(argv[1], argv, NULL) == -1)
-		{
-			perror("Error");
-			exit(-1);
-		}
-		return (0);
-	}
-
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(1);
-	}
-
-	do {
-		if (isatty(fileno(stdin)))
-		{
-			isPipe = 1;
-			_puts("cisfun#: ");
-		}
-
-		getline(&buffer, &bufsize, stdin);
-		buffer[_strlen(buffer) - 1] = '\0';
-		tokens = stringToTokens(buffer);
-		response = execute(tokens);
-	} while (isPipe && response != -1);
-
-	return (0);
+    signal(SIGINT, SIG_IGN);
+    PATH = _getenv("PATH");
+    if (PATH == NULL)
+        return (-1);
+    while (1)
+    {
+        av = NULL;
+        prompt();
+        buffer = _read();
+        if (*buffer != '\0')
+        {
+            av = tokenize(buffer);
+            if (av == NULL)
+            {
+                free(buffer);
+                continue;
+            }
+            fullpathbuffer = _fullpathbuffer(av, PATH, copy);
+            if (checkbuiltins(av, buffer, exitstatus) == 1)
+                continue;
+            exitstatus = _forkprocess(av, buffer, fullpathbuffer);
+        }
+        else
+            free(buffer);
+    }
+    return (0);
 }
